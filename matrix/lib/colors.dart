4053795e2dd7b77f6004f9f1918552bf71fae102
AnujGupta-bit <guptaanuj776@gmail.com>
2024-11-20 01:02:47 +0530

    Delete matrix directory

--- matrix/lib/colors.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-const OnBoardingPage1Color = Colors.pinkAccent;
-const OnBoardingPage2Color = Colors.cyan;
-const OnBoardingPage3Color = Colors.grey;
-
--- matrix/lib/imgString.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter/material.dart';
-const String OnBoardImage1 = "wallpaper.jpg";
-const String OnBoardImage2 = "wallpaper.jpg";
-const String OnBoardImage3 = "wallpaper.jpg";
